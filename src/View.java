73604105c7e7b5014d4f405e3ce8e1e9b3d3bc36
sirus20x6 <sirus20x6@gmail.com>
2015-02-06 10:49:48 -0600

    sync2

--- src/View.java
+++ src/View.java
@@ -811,33 +811,6 @@ public class View implements Runnable, GLEventListener
         /***********************************************************************/
 
 
-        String path1 = "models/formula.obj";
-
-        try{
-/*            texture_manager.createManagedTexture
-                    ("road", "textures/roads/road.jpg",
-                            GL.GL_TEXTURE_2D, GL.GL_RGB, GL.GL_RGB, GL.GL_LINEAR,
-                            GL.GL_LINEAR, true, true );*/
-            // ...
-            //in the same way we load each texture is needed...
-            //...
-
-            //a file input stream reads the data and stores them in
-            //a buffer reader for each 3D model
-            FileInputStream r_path1 = new FileInputStream(path1);
-            BufferedReader b_read1 =
-                    new BufferedReader(new InputStreamReader(r_path1));
-            model1 = new GLModel(b_read1, true, "models/formula.mtl", gl);
-
-
-
-            r_path1.close();
-
-        }
-        catch( Exception e ){
-            System.out.println("LOADING ERROR");
-        }
-
 
 
 
